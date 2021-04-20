fe1ab793c4b0ef113018b5653ca505d814214c9e
Michael Russo <mjrusso@gmail.com>
2021-04-20 08:47:17 -0400

    Initial Commit

---
+++ Scoot/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
