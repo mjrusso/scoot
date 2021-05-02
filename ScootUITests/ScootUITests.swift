27286254d25157d07d8d402d6461b1633cd3e6ef
Michael Russo <mjrusso@gmail.com>
2021-05-02 12:43:19 -0400

    Remove unused tests.

--- ScootUITests/ScootUITests.swift
+++ ScootUITests/ScootUITests.swift
@@ -1,8 +1,8 @@
-//
-
 import XCTest
 
 class ScootUITests: XCTestCase {
+
+    /*
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -34,4 +34,7 @@ class ScootUITests: XCTestCase {
             }
         }
     }
+
+     */
+
 }
