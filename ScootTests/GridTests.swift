8ab770c11abc9d79c3f3e17587c0521094b7ff62
Michael Russo <mjrusso@gmail.com>
2021-05-02 13:02:55 -0400

    Add additional grid-related tests.

--- ScootTests/GridTests.swift
+++ ScootTests/GridTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class GridTests: XCTestCase {
 
-    let grid = Grid(numRows: 3, numCols: 4, gridSize: CGSize(width: 200, height: 200))
+    let grid = Grid(numRows: 3, numCols: 4, gridSize: .zero)
 
     func testIndexing() {
         XCTAssertEqual(grid.index(atX: 0, y: 0), 0)
@@ -56,6 +56,48 @@ class GridTests: XCTestCase {
 
         XCTAssertEqual(grid.coordinates(atIndex: 11).x, 3)
         XCTAssertEqual(grid.coordinates(atIndex: 11).y, 2)
+    }
+
+    func testBasicInitialization() {
+        let numRows = 3
+        let numCols = 4
+        let gridSize = CGSize(width: 200, height: 400)
+
+        let grid = Grid(numRows: numRows, numCols: numCols, gridSize: gridSize)
+
+        XCTAssertEqual(grid.numCells, numRows*numCols)
+        XCTAssertEqual(grid.cellWidth, gridSize.width/CGFloat(numCols))
+        XCTAssertEqual(grid.cellHeight, gridSize.height/CGFloat(numRows))
+        XCTAssertEqual(grid.cellSize, CGSize(
+                        width: gridSize.width/CGFloat(numCols),
+                        height: gridSize.height/CGFloat(numRows)))
+    }
+
+    func testInitializationViaTargetSizing() {
+        let gridSize = CGSize(width: 200, height: 440)
+
+        // This target size evenly divides the grid size, in both dimensions.
+        var targetCellSize = CGSize(width: 20, height: 40)
+
+        var grid = Grid(gridSize: gridSize, targetCellSize: targetCellSize)
+
+        XCTAssertEqual(grid.numColumns, 10)
+
+        XCTAssertEqual(grid.numRows, 11)
+
+        XCTAssertEqual(grid.cellSize, targetCellSize)
+
+        // This target size doesn't evenly divide the grid size, in either dimension.
+        targetCellSize = CGSize(width: 30, height: 30)
+
+        grid = Grid(gridSize: gridSize, targetCellSize: targetCellSize)
+
+        XCTAssertEqual(grid.numColumns, 6)
+        XCTAssertEqual(grid.numRows, 14)
+
+        XCTAssertEqual(grid.cellSize, CGSize(
+                        width: gridSize.width/CGFloat(6),
+                        height: gridSize.height/CGFloat(14)))
     }
 
 }
