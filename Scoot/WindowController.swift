c88633bdcb511a0fa1ca7f336460ea3097f64e03
Michael Russo <mjrusso@gmail.com>
2021-05-21 13:46:50 -0400

    Reset state when Scoot moves to the background.

--- Scoot/WindowController.swift
+++ Scoot/WindowController.swift
@@ -23,6 +23,7 @@ extension WindowController: NSWindowDelegate {
     // Automatically hide the grid UI when Scoot is not in the foreground.
     func windowDidResignMain(_ notification: Notification) {
         viewController.hideGrid()
+        viewController.currentNode = nil
     }
 
     // Automatically show the grid UI when Scoot enters the foreground.
