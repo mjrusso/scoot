cc17d5e2e5c3643b31302822561ec574483e190c
Michael Russo <mjrusso@gmail.com>
2021-05-24 19:30:29 -0400

    Menu: display "show grid" item when grid is in hidden state.
    (And, likewise, display the "hide grid" item when the grid is visible.)

--- Scoot/WindowController.swift
+++ Scoot/WindowController.swift
@@ -2,6 +2,10 @@ import Cocoa
 
 class WindowController: NSWindowController {
 
+    var appDelegate: AppDelegate? {
+        NSApp.delegate as? AppDelegate
+    }
+
     var viewController: ViewController {
         contentViewController as! ViewController
     }
@@ -24,11 +28,17 @@ extension WindowController: NSWindowDelegate {
     func windowDidResignMain(_ notification: Notification) {
         viewController.hideGrid()
         viewController.currentNode = nil
+
+        appDelegate?.showMenuItem.isHidden = false
+        appDelegate?.hideMenuItem.isHidden = true
     }
 
     // Automatically show the grid UI when Scoot enters the foreground.
     func windowDidBecomeMain(_ notification: Notification) {
         viewController.showGrid()
+
+        appDelegate?.showMenuItem.isHidden = true
+        appDelegate?.hideMenuItem.isHidden = false
     }
 
 }
