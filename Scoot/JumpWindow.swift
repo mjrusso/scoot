cf129cece1ece40e747c99c846d5669ade5f0ae1
Michael Russo <mjrusso@gmail.com>
2022-02-01 22:48:55 -0500

    Properly support Spaces.
    (And, as a bonus, hide Scoot from Exposé.)
    
    For posterity, the following resource helped:
    https://stackoverflow.com/questions/33845596/window-visible-on-all-spaces-including-other-fullscreen-apps

--- Scoot/JumpWindow.swift
+++ Scoot/JumpWindow.swift
@@ -2,4 +2,10 @@ import Cocoa
 
 class JumpWindow: TransparentWindow {
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        level = .floating
+    }
+
 }
--- Scoot/TransparentWindow.swift
+++ Scoot/TransparentWindow.swift
@@ -10,6 +10,15 @@ class TransparentWindow: NSWindow {
         isOpaque = false
         isMovable = false
         ignoresMouseEvents = true
+
+        collectionBehavior = [
+            // When the window becomes active, move it to the active space
+            // instead of switching spaces.
+            .moveToActiveSpace,
+
+            // The window floats in Spaces and hides in Exposé.
+            .transient,
+        ]
     }
 
 }
